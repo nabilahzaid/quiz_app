600b38d2e753507fa87518034be3f4a2e02651fc
Nabilah Zaid <nabilah.zaid@gmail.com>
2020-09-26 08:37:09 +0800

    finish simple quiz

--- lib/main.dart
+++ lib/main.dart
@@ -19,25 +19,49 @@ class MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'Who\'s the best cat?',
-      'answer': ['Kiki', 'Momo', 'Koko', 'Mimi']
+      'answer': [
+        {'text': 'Kiki', 'score': 6},
+        {'text': 'Momo', 'score': 10},
+        {'text': 'Koko', 'score': 4},
+        {'text': 'Mimi', 'score': 3}
+      ]
     },
     {
       'questionText': 'Whats\' your favourite food?',
-      'answer': ['Spaghetti', 'Sushi', 'Kimbap', 'Nasi Lemak']
+      'answer': [
+        {'text': 'Spaghetti', 'score': 4},
+        {'text': 'Sushi', 'score': 5},
+        {'text': 'Kimbap', 'score': 8},
+        {'text': 'Nasi Lemak', 'score': 1}
+      ]
     },
     {
       'questionText': 'Whats\' your favourite colour?',
-      'answer': ['Pink', 'Blue', 'White', 'Black']
+      'answer': [
+        {'text': 'Pink', 'score': 2},
+        {'text': 'Blue', 'score': 4},
+        {'text': 'White', 'score': 7},
+        {'text': 'Black', 'score': 6}
+      ]
     }
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      _questionIndex += 1;
+    });
     if (_questionIndex < _questions.length) {
       print('we have more question');
-      setState(() {
-        _questionIndex += 1;
-      });
     } else {
       _questionIndex = _questions.length;
     }
@@ -57,7 +81,7 @@ class MyAppState extends State<MyApp> {
               answerQuestion: _answerQuestion,
               questionIndex: _questionIndex,
               questions: _questions)
-          : Result(),
+          : Result(_totalScore, _resetQuiz),
     ));
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+
+    if (resultScore <= 6) {
+      resultText = 'Wow, you are amazing!';
+    } else if (resultScore <= 10) {
+      resultText = 'Kawaii person on the planet!';
+    } else {
+      resultText = 'Daijobudayo??';
+    }
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(onPressed: resetHandler, child: Text('Restart Quiz!'))
+        ],
+      ),
     );
   }
 }
